0b279478dabff284821ffe76da56208f12700a0b
omerh <omerha@protonmail.com>
2024-01-28 14:08:20 +0200

    runtime container

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Matterbridge runtime 
+#
+# set -x
+# Environment variables:
+# phoneNumber e.g 972505152183
+# userStorage e.g. s3://us-west-2-guardian-data154316-dev/private/us-west-2:6da317cd-7edd-472d-8d3c-6aee1296ac3c/phone-data/${phoneNumber}/
+
+# files
+phoneNumber="972525583454"
+CONF_FILE="matterbridge.toml"
+SESSION_FILE="session+${phoneNumber}.gob.db"
+FILE_DESTINATION="/etc/matterbridge/"
+userStorage="s3://us-west-2-guardian-data154316-dev/private/us-west-2:6da317cd-7edd-472d-8d3c-6aee1296ac3c/phone-data/${phoneNumber}"
+
+# download from S3
+donwload_from_s3() {
+    echo "Downloading $1 to ${userStorage}"
+    aws s3 cp --profile c $userStorage/$1 $FILE_DESTINATION/$1
+}
+
+# environment debug
+echo "working on phoneNumber: $phoneNumber"
+echo "bucket: $userStorage"
+echo "session file: ${SESSION_FILE}"
+
+# Download config, and session db file from s3 userStorage
+donwload_from_s3 ${CONF_FILE}
+download_from_s3 ${SESSION_FILE}
+
+echo "Starting matterbrigde"
+# /etc/matterbridge/matterbridge
+exec "$@"
+
