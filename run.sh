046b73a58b59719e51682f423881808b8faa4d37
omerh <omerha@protonmail.com>
2024-01-28 15:57:26 +0200

    typos

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ FILE_DESTINATION="/etc/matterbridge/"
 # userStorage="s3://us-west-2-guardian-data154316-dev/private/us-west-2:6da317cd-7edd-472d-8d3c-6aee1296ac3c/phone-data/${phoneNumber}"
 
 # download from S3
-donwload_from_s3() {
+download_from_s3() {
     echo "Downloading $1 to ${userStorage}"
     aws s3 cp --profile c $userStorage/$1 $FILE_DESTINATION/$1
 }
@@ -26,7 +26,7 @@ echo "bucket: $userStorage"
 echo "session file: ${SESSION_FILE}"
 
 # Download config, and session db file from s3 userStorage
-donwload_from_s3 ${CONF_FILE}
+download_from_s3 ${CONF_FILE}
 download_from_s3 ${SESSION_FILE}
 
 echo "Starting matterbrigde"
